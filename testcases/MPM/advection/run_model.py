86ed46e7f80c875680e5170dd086896049410dad
Adrian K. Turner <akt@lanl.gov>
2023-10-27 15:03:30 -0500

    Add changes to advection test case for parallelism
    Added changes to MPM/advection test case to check for BFB sameness when running on multiple processors
    Also add script to plot results of test case

--- testcases/MPM/advection/run_model.py
+++ testcases/MPM/advection/run_model.py
@@ -3,16 +3,13 @@ import argparse
 
 #-------------------------------------------------------------------------------
 
-def run_model(nCells):
+def run_model(nCells, nProcs):
 
     MPAS_SEAICE_EXECUTABLE = os.environ.get('MPAS_SEAICE_EXECUTABLE')
     if (MPAS_SEAICE_EXECUTABLE is None):
         raise Exception("MPAS_SEAICE_EXECUTABLE must be set")
 
-    if (not os.path.isdir("output")):
-        os.mkdir("output")
-
-    cmd = "rm grid.nc ic.nc"
+    cmd = "rm grid.nc ic.nc log.seaice.*"
     print(cmd)
     os.system(cmd)
 
@@ -24,7 +21,20 @@ def run_model(nCells):
     print(cmd)
     os.system(cmd)
 
-    cmd = "%s" %(MPAS_SEAICE_EXECUTABLE)
+    if (nProcs > 1):
+        os.chdir("./graphs")
+
+        cmd = "ln -s graph.%i.info.part.%i graph.info.part.%i" %(nCells, nProcs, nProcs)
+        print(cmd)
+        os.system(cmd)
+
+        os.chdir("..")
+
+    cmd = "mpirun -np %i %s" %(nProcs, MPAS_SEAICE_EXECUTABLE)
+    print(cmd)
+    os.system(cmd)
+
+    cmd = "mv output output_%i" %(nProcs)
     print(cmd)
     os.system(cmd)
 
@@ -33,7 +43,8 @@ def run_model(nCells):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', dest="nCells", type=int)
+    parser.add_argument('-c', dest="nCells", type=int, required=True)
+    parser.add_argument('-p', dest="nProcs", type=int, required=True)
     args = parser.parse_args()
 
-    run_model(args.nCells)
+    run_model(args.nCells, args.nProcs)
