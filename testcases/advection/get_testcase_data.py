65b2095c50e71adb68731e6037a123a7c9e660a4
Adrian K. Turner <akt@lanl.gov>
2025-05-05 10:54:51 -0500

    Moved location of standalone test case data
    Test case input data is now on perlmutter
    Instead of wgetting the data everytime the test cases are run, a tared file is downloaded and extracted once.
    Instructions added to a README

--- testcases/advection/get_testcase_data.py
+++ testcases/advection/get_testcase_data.py
@@ -5,35 +5,27 @@ import os
 
 def get_testcase_data():
 
-    nCellsArray = [10242, 163842, 2562, 40962]
+    MPAS_SEAICE_STANDALONE_DATA = os.environ.get('MPAS_SEAICE_STANDALONE_DATA')
+    if (MPAS_SEAICE_STANDALONE_DATA is None):
+        raise Exception("MPAS_SEAICE_STANDALONE_DATA must be set")
 
-    dirName = "https://web.lcrc.anl.gov/public/e3sm/mpas_standalonedata/mpas-seaice/testcases/strain_stress_divergence/"
+    nCellsArray = [10242, 163842, 2562, 40962]
 
     for nCells in nCellsArray:
 
         # grid file
-        filename = "grid.%i.nc" %(nCells)
+        filanameDst = "grid.%i.nc" %(nCells)
+        filenameSrc = "%s/testcases/strain_stress_divergence/%s" %(MPAS_SEAICE_STANDALONE_DATA,filanameDst)
 
-        if (not os.path.isfile(filename)):
-
-            args = ["wget", dirName+filename]
-
-            process = subprocess.Popen(args, stdout=subprocess.PIPE)
-
-            while process.poll() is None:
-                line = process.stdout.readline()
-
+        if (not os.path.isfile(filanameDst)):
+            os.symlink(filenameSrc, filanameDst)
+        
         # graph file
-        filename = "graph.%i.info" %(nCells)
+        filanameDst = "graph.%i.info" %(nCells)
+        filenameSrc = "%s/testcases/strain_stress_divergence/%s" %(MPAS_SEAICE_STANDALONE_DATA,filanameDst)
 
-        if (not os.path.isfile(filename)):
-
-            args = ["wget", dirName+filename]
-
-            process = subprocess.Popen(args, stdout=subprocess.PIPE)
-
-            while process.poll() is None:
-                line = process.stdout.readline()
+        if (not os.path.isfile(filanameDst)):
+            os.symlink(filenameSrc, filanameDst)
 
 #-------------------------------------------------------------------------------
 
