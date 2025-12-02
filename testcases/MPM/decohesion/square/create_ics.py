0d334eefdc602d34421f229e97fca87a54d834aa
sulsky <sulsky@math.unm.edu>
2025-12-02 13:38:37 -0700

    Clean up the testcase scripts for decoheions on a square, planar domain with wind forcing.

--- testcases/MPM/decohesion/square/create_ics.py
+++ testcases/MPM/decohesion/square/create_ics.py
@@ -1,9 +1,5 @@
 from netCDF4 import Dataset
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-from matplotlib.collections import PatchCollection
-from matplotlib.patches import Polygon
 import argparse
 
 #-------------------------------------------------------------------------------
@@ -17,38 +13,11 @@ def create_ics(gridFilename):
     fileGrid = Dataset(gridFilename,"r")
 
     nCells = len(fileGrid.dimensions["nCells"])
-    nVertices = len(fileGrid.dimensions["nVertices"])
 
     xCell = fileGrid.variables["xCell"][:]
     yCell = fileGrid.variables["yCell"][:]
 
-    xVertex = fileGrid.variables["xVertex"][:]
-    yVertex = fileGrid.variables["yVertex"][:]
-
-    verticesOnCell = fileGrid.variables["verticesOnCell"][:] - 1
-    cellsOnCell = fileGrid.variables["cellsOnCell"][:] - 1
-
     fileGrid.close()
-
-    xmin = np.amin(xVertex)
-    xmax = np.amax(xVertex)
-    ymin = np.amin(yVertex)
-    ymax = np.amax(yVertex)
-    dx = xmax - xmin
-    dy = ymax - ymin
-
-    nCategories = 1
-
-    iceAreaCategory = np.zeros((nCells,nCategories))
-    iceVolumeCategory = np.zeros((nCells,nCategories))
-
-    iceAreaCell = np.zeros((nCells))
-    iceVolumeCell = np.zeros((nCells))
-
-    iceThicknesses = np.zeros(nCategories)
-    for iCategory in range(0,nCategories-1):
-        iceThicknesses[iCategory] = 1.0
-    #print(iceThicknesses)
 
     uAirVelocity = np.zeros(nCells)
     vAirVelocity = np.zeros(nCells)
@@ -63,37 +32,9 @@ def create_ics(gridFilename):
         if (x < 120000):
            uAirVelocity[iCell] = -uAir
 
-        if (x < 160000 and x > 80000 and y < 160000 and y > 80000):
-            for iCategory in range(0,nCategories):
-                iceAreaCategory[iCell,iCategory] = 1.0
-            iceAreaCell[iCell] = np.sum(iceAreaCategory[iCell,:])
-
-            for iCategory in range(0,nCategories):
-                iceVolumeCategory[iCell,iCategory] = iceThicknesses[iCategory] * iceAreaCategory[iCell,iCategory]
-            iceVolumeCell[iCell] = np.sum(iceVolumeCategory[iCell,:])
-
     fileOut = Dataset("ic.nc", "w", format="NETCDF3_CLASSIC")
 
     fileOut.createDimension("nCells", nCells)
-    fileOut.createDimension("nVertices", nVertices)
-    fileOut.createDimension("nCategories", nCategories)
-    fileOut.createDimension("ONE", 1)
-
-    var = fileOut.createVariable("iceAreaCategory", "d", dimensions=["nCells","nCategories","ONE"])
-    var[:,:,0] = iceAreaCategory[:,:]
-
-    var = fileOut.createVariable("iceVolumeCategory", "d", dimensions=["nCells","nCategories","ONE"])
-    var[:,:,0] = iceVolumeCategory[:,:]
-
-    var = fileOut.createVariable("iceAreaCell", "d", dimensions=["nCells"])
-    var[:] = iceAreaCell[:]
-
-    var = fileOut.createVariable("iceVolumeCell", "d", dimensions=["nCells"])
-    var[:] = iceVolumeCell[:]
-
-    fVertex = 1.46e-4
-    var = fileOut.createVariable("fVertex", "d", dimensions=["nVertices"])
-    var[:] = fVertex
 
     var = fileOut.createVariable("uAirVelocity", "d", dimensions=["nCells"])
     var[:] = uAirVelocity
@@ -102,62 +43,6 @@ def create_ics(gridFilename):
     var[:] = vAirVelocity
 
     fileOut.close()
-
-    # plots
-    patches = []
-    for iCell in range(0,nCells):
-        vertices = []
-        for iVertexOnCell in range(0,4):
-            iVertex = verticesOnCell[iCell,iVertexOnCell]
-            vertices.append((xVertex[iVertex],yVertex[iVertex]))
-        patches.append(Polygon(vertices, closed=True, edgecolor="teal", fill=False, linewidth=0.1))
-
-    xMin = np.amin(xVertex)
-    xMax = np.amax(xVertex)
-    yMin = np.amin(yVertex)
-    yMax = np.amax(yVertex)
-
-    # iceAreaCell
-    fig, axis = plt.subplots()
-
-    pc = PatchCollection(patches, cmap="jet")
-    pc.set_array(np.array(iceAreaCell))
-    axis.add_collection(pc)
-
-    divider = make_axes_locatable(axis)
-    cax = divider.append_axes('right', size='5%', pad=0.05)
-    cb = fig.colorbar(pc, cax=cax)
-
-    axis.set_aspect("equal")
-
-    axis.set_xlim((xMin,xMax))
-    axis.set_ylim((yMin,yMax))
-
-    plt.tight_layout()
-    plt.savefig("iceAreaCell.png",dpi=300)
-    plt.cla()
-    plt.close(fig)
-
-    # iceVolumeCell
-    fig, axis = plt.subplots()
-
-    pc = PatchCollection(patches, cmap="jet")
-    pc.set_array(np.array(iceVolumeCell))
-    axis.add_collection(pc)
-
-    divider = make_axes_locatable(axis)
-    cax = divider.append_axes('right', size='5%', pad=0.05)
-    cb = fig.colorbar(pc, cax=cax)
-
-    axis.set_aspect("equal")
-
-    axis.set_xlim((xMin,xMax))
-    axis.set_ylim((yMin,yMax))
-
-    plt.tight_layout()
-    plt.savefig("iceVolumeCell.png",dpi=300)
-    plt.cla()
-    plt.close(fig)
 
 #-------------------------------------------------------------------------------
 
--- testcases/MPM/decohesion/square/run_testcase.py
+++ testcases/MPM/decohesion/square/run_testcase.py
@@ -4,18 +4,18 @@ from create_ics import create_ics
 import sys, os
 
 sys.path.append("../../../../utils/MPM/particle_initialization/")
-from create_particles_from_cell_file import create_particles_from_cell_file
 from initial_particle_positions import initial_particle_positions
 
 sys.path.append("../../../../utils/testcases")
 from create_square_hex_mesh import create_square_hex_mesh
+from create_square_quad_mesh import create_square_quad_mesh
 from plot_mesh import plot_mesh
 
 import argparse
 
 #-------------------------------------------------------------------------------
 
-def run_testcase(logFilename=None):
+def run_testcase(meshtype, logFilename=None):
 
     if (logFilename is not None):
         logFile = open(logFilename,"a")
@@ -27,12 +27,26 @@ def run_testcase(logFilename=None):
 
     print("Create grid")
     print("=================")
-    lx = 240000.0
-    ly = 240000.0
-    x0 = 0.0
-    y0 = 0.0
-    dc = 10000.0
-    gridFilename = create_square_hex_mesh(dc,lx,ly,x0,y0)
+    if (meshtype == 'hex'):
+        lx = 240000.0
+        ly = 240000.0
+        x0 = 0.0
+        y0 = 0.0
+        dc = 10000.0
+        gridFilename = create_square_hex_mesh(dc,
+                                          lx, ly,
+                                          x0, y0)
+    elif (meshtype == 'quad'):
+        lx = 240000.0
+        ly = 240000.0
+        x0 = 0.0
+        y0 = 0.0
+        nx = 24
+        ny = 24
+        gridFilename = create_square_quad_mesh(nx, ny,
+                                           lx, ly,
+                                           x0, y0)
+
     plot_mesh(gridFilename)
 
     print("Sym link grid...")
@@ -41,22 +55,37 @@ def run_testcase(logFilename=None):
     os.symlink(gridFilename,"grid.nc")
 
     print("Create ICs...")
+    print("=================")
     create_ics(gridFilename)
 
     print("Create particles...")
-    particleInitType = "number"
-    particleInitNumber = "9"
-    particlePositionInitType = "onePerEdge"
-    particleGeometry = "square"
-    sphereRadius = 1.0
-    initial_particle_positions(gridFilename,
+    print("=================")
+    if (meshtype == 'hex'):
+       particleInitType = "number"
+       particleInitNumber = "9"
+       particlePositionInitType = "onePerEdge"
+       particleGeometry = "square"
+       sphereRadius = 1.0
+       initial_particle_positions(gridFilename,
                                 "particles.nc",
                                 particleInitType,
                                 particleInitNumber,
                                 particlePositionInitType,
                                 particleGeometry,
                                 sphereRadius)
-
+    elif (meshtype == 'quad'):
+       particleInitType = "number"
+       particleInitNumber = "4"
+       particlePositionInitType = "even"
+       particleGeometry = "square"
+       sphereRadius = 1.0
+       initial_particle_positions(gridFilename,
+                                "particles.nc",
+                                particleInitType,
+                                particleInitNumber,
+                                particlePositionInitType,
+                                particleGeometry,
+                                sphereRadius)
 
     print("Run model")
     print("=================")
@@ -71,8 +100,9 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
+    parser.add_argument('-m', dest="meshtype", choices=["quad","hex"], default="quad")
     parser.add_argument('-l', dest='logFilename')
 
     args = parser.parse_args()
 
-    run_testcase(args.logFilename)
+    run_testcase(args.meshtype, args.logFilename)
