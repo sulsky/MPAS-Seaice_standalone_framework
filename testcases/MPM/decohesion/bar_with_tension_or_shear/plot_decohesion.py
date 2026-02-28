3d799f623957e55cd859c64fb11b106308400f95
sulsky <sulsky@math.unm.edu>
2026-02-28 15:35:22 -0700

    Remove crack tracking arrays from plots and streams files.

--- testcases/MPM/decohesion/bar_with_tension_or_shear/plot_decohesion.py
+++ testcases/MPM/decohesion/bar_with_tension_or_shear/plot_decohesion.py
@@ -44,8 +44,6 @@ def plot_decohesion(gridFilename):
         nParticles = len(filein.dimensions["nParticles"])
         posnMP = filein.variables["posnMP"][:,:,:]
         decohesionOpeningMP = filein.variables["decohesionOpeningMP"][:,:,:]
-        decohesionCrackEndPoints = filein.variables["decohesionCrackEndPoints"][:,:,:]
-        decohesionCrackEdgeIndex = filein.variables["decohesionCrackEdgeIndex"][:,:,:]
         filein.close()
 
         # plot decohesionOpeningMP at posnMP
@@ -60,42 +58,6 @@ def plot_decohesion(gridFilename):
         plt.colorbar(scatter)
         plt.tight_layout()
         filenameOut = "plots/decohesion_%i.png"%(iTime)
-        plt.savefig(filenameOut,dpi=600)
-        plt.close()
-
-        # plot the mesh and cracks
-        patchesCell = []
-        for iCell in range(0,nCells):
-           vertices = []
-           for iVertexOnCell in range(0,nEdgesOnCell[iCell]):
-               iVertex = verticesOnCell[iCell,iVertexOnCell]
-               vertices.append((xVertex[iVertex],yVertex[iVertex]))
-           patchesCell.append(Polygon(vertices,closed=True, edgecolor="teal", fill=False, linewidth=0.1))
-
-        pc = PatchCollection(patchesCell, match_original=True)
-
-        crackLines = []
-        for iCell in range(0, nCells):
-           crackIndex = decohesionCrackEdgeIndex[0,iCell,:] - 1
-           if (crackIndex[0] >= 0 and crackIndex[1] >=0):
-              iEdges = edgesOnCell[iCell, crackIndex]
-              crackLines.append([(decohesionCrackEndPoints[0,iEdges[0],0],decohesionCrackEndPoints[0,iEdges[0],1]),
-                                 (decohesionCrackEndPoints[0,iEdges[1],0],decohesionCrackEndPoints[0,iEdges[1],1])])
-
-        lc = LineCollection(crackLines)
-
-        fig, axis = plt.subplots(figsize=(10,10))
-        axis.add_collection(pc)
-        axis.add_collection(lc)
-        axis.scatter(decohesionCrackEndPoints[0,:,0], decohesionCrackEndPoints[0,:,1], marker='o')
-        axis.set_xlim((xmin-0.05*lxy,xmax+0.05*lxy))
-        axis.set_ylim((ymin-0.05*lxy,ymax+0.05*lxy))
-        axis.set_aspect('equal')
-        axis.set_xlabel("x")
-        axis.set_ylabel("y")
-
-        plt.tight_layout()
-        filenameOut = "plots/cracks_%i.png"%(iTime)
         plt.savefig(filenameOut,dpi=600)
         plt.close()
 
--- testcases/MPM/decohesion/compression/plot_results.py
+++ testcases/MPM/decohesion/compression/plot_results.py
@@ -37,8 +37,6 @@ def plot_results(gridFilename):
     posnMP = filein.variables["posnMP"][:,:,:]
     uvVelMP = filein.variables["uvVelMP"][:,:,:]
     decohesionOpeningMP = filein.variables["decohesionOpeningMP"][:,:,:]
-    decohesionCrackEndPoints = filein.variables["decohesionCrackEndPoints"][:,:,:]
-    decohesionCrackEdgeIndex = filein.variables["decohesionCrackEdgeIndex"][:,:,:]
     filein.close()
 
     # plot decohesionOpeningMP at posnMP
@@ -101,42 +99,6 @@ def plot_results(gridFilename):
 
     plt.tight_layout()
     filenameOut = "decohesion.png"
-    plt.savefig(filenameOut,dpi=600)
-    plt.close()
-
-    # plot the mesh and cracks
-    patchesCell = []
-    for iCell in range(0,nCells):
-       vertices = []
-       for iVertexOnCell in range(0,nEdgesOnCell[iCell]):
-           iVertex = verticesOnCell[iCell,iVertexOnCell]
-           vertices.append((xVertex[iVertex],yVertex[iVertex]))
-       patchesCell.append(Polygon(vertices,closed=True, edgecolor="teal", fill=False, linewidth=0.1))
-
-    pc = PatchCollection(patchesCell, match_original=True)
-
-    crackLines = []
-    for iCell in range(0, nCells):
-       crackIndex = decohesionCrackEdgeIndex[0,iCell,:] - 1
-       if (crackIndex[0] >= 0 and crackIndex[1] >=0):
-          iEdges = edgesOnCell[iCell, crackIndex]
-          crackLines.append([(decohesionCrackEndPoints[0,iEdges[0],0],decohesionCrackEndPoints[0,iEdges[0],1]),
-                             (decohesionCrackEndPoints[0,iEdges[1],0],decohesionCrackEndPoints[0,iEdges[1],1])])
-
-    lc = LineCollection(crackLines)
-
-    fig, axis = plt.subplots(figsize=(10,10))
-    axis.add_collection(pc)
-    axis.add_collection(lc)
-    axis.scatter(decohesionCrackEndPoints[0,:,0], decohesionCrackEndPoints[0,:,1], marker='o')
-    axis.set_xlim((xmin-0.05*lxy,xmax+0.05*lxy))
-    axis.set_ylim((ymin-0.05*lxy,ymax+0.05*lxy))
-    axis.set_aspect('equal')
-    axis.set_xlabel("x")
-    axis.set_ylabel("y")
-
-    plt.tight_layout()
-    filenameOut = "cracks.png"
     plt.savefig(filenameOut,dpi=600)
     plt.close()
 
